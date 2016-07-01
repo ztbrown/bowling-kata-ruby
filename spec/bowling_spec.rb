43512070026d503a02a2405ee7cca5061f986749
Zack Danger Brown <zackdangerbrown@gmail.com>
2016-07-01 06:57:58 -0400

    gutter ball

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -9,21 +9,37 @@ RSpec.describe Bowling, "#score" do
     end
   end
 
+  context "following a gutterball" do
+    it "records a score of 0 when no pins are knocked down" do
+      expect(bowling.score("-")).to eq(0)
+    end
+
+    (1..9).to_a.permutation(2).each do |pins|
+      it "records a score of #{pins[0] + pins[1]} when #{pins[0]} and #{pins[1]} pins are knocked down on the next two rolls" do
+        expect(bowling.score("-#{pins[0].to_s}#{pins[1].to_s}")).to eq(pins[0] + pins[1])
+      end
+    end
+  end
+
   context 'following a strike' do
     (1..9).to_a.permutation(2).each do |pins|
       it "records a score of #{pins[0] + pins[1] + (10 + pins[0] + pins[1])} when #{pins[0]} and #{pins[1]} pins are knocked down on the next two rolls" do
         expect(bowling.score("X#{pins[0].to_s}#{pins[1].to_s}")).to eq(pins[0] + pins[1] + (10 + pins[0] + pins[1]))
       end
     end
+
     it "records a score of 60 for 3 strikes" do
       expect(bowling.score("XXX")).to eq(60)
     end
+
     it "records a score of 30 for a spare following a strike" do
       expect(bowling.score("X/")).to eq(30)
     end
+
     it "records a score of 300 for a perfect game" do
       expect(bowling.score("XXXXXXXXXXXX")).to eq(300)
     end
+
     it "records a score of 274 for a near-perfect game" do
       expect(bowling.score("XXXXXXXXXX12")).to eq(274)
     end
--- src/bowling.rb
+++ src/bowling.rb
@@ -13,5 +13,3 @@ class Bowling
     }.reduce(:+)
   end
 end
-
-#(frames.scan /./).map.with_index {|b, i| b == "X" ? b + frames[i+1].to_s + frames[i+2].to_s : b}.map.with_index {|x, i| x == "/" ? x + frames[i+1].to_s : x}.map { |c| (c.scan /./).map {|d| d == "X" ? 10 : d}.map {|e| e == "/" ? 10 : e.to_i}.reduce(:+)}.reduce(:+)
